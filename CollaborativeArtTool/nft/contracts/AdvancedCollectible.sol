4c384c57ab2cf16aa85dcf94c7631aca99f9311c
Gilberto Guadiana <gilbertoguadiana2022@u.northwestern.edu>
2021-07-02 13:58:33 -0500

    changed class name

--- CollaborativeArtTool/nft/contracts/AdvancedCollectible.sol
+++ CollaborativeArtTool/nft/contracts/AdvancedCollectible.sol
@@ -1,50 +1,34 @@
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract AdvancedCollectible is ERC721, VRFConsumerBase {
+contract AdvancedCollectible is ERC721 {
     uint256 public tokenCounter;
-    enum Breed{PUG, SHIBA_INU, ST_BERNARD}
     // add other things
     mapping(bytes32 => address) public requestIdToSender;
     mapping(bytes32 => string) public requestIdToTokenURI;
     mapping(uint256 => Breed) public tokenIdToBreed;
     mapping(bytes32 => uint256) public requestIdToTokenId;
-    event requestedCollectible(bytes32 indexed requestId); 
+    event requestedCollectible(bytes32 indexed requestId);
 
 
     bytes32 internal keyHash;
     uint256 internal fee;
-    
+
     constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash)
-    public 
+    public
     VRFConsumerBase(_VRFCoordinator, _LinkToken)
-    ERC721("Dogie", "DOG")
+    ERC721("CollaborativeArt", "CART")
     {
         tokenCounter = 0;
-        keyHash = _keyhash;
-        fee = 0.1 * 10 ** 18;
     }
 
-    function createCollectible(string memory tokenURI, uint256 userProvidedSeed) 
+    function createCollectible(string memory tokenURI, uint256 userProvidedSeed)
         public returns (bytes32){
             bytes32 requestId = requestRandomness(keyHash, fee, userProvidedSeed);
             requestIdToSender[requestId] = msg.sender;
             requestIdToTokenURI[requestId] = tokenURI;
             emit requestedCollectible(requestId);
-    }
-
-    function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
-        address dogOwner = requestIdToSender[requestId];
-        string memory tokenURI = requestIdToTokenURI[requestId];
-        uint256 newItemId = tokenCounter;
-        _safeMint(dogOwner, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        Breed breed = Breed(randomNumber % 3); 
-        tokenIdToBreed[newItemId] = breed;
-        requestIdToTokenId[requestId] = newItemId;
-        tokenCounter = tokenCounter + 1;
     }
 
     function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
