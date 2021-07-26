6ecc71536f1dc82b205d2e1bc6f0c10c3c1c5b21
Gilberto Guadiana <gilbertoguadiana2022@u.northwestern.edu>
2021-07-26 17:04:12 -0500

    made burnable and added tests

--- CollaborativeArtToolv2/nft/src/contracts/SimpleCollectible.sol
+++ CollaborativeArtToolv2/nft/src/contracts/SimpleCollectible.sol
@@ -3,14 +3,20 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract SimpleCollectible is ERC721URIStorage { // NFT Smart Contract Constructor
+// NFT Smart Contract Constructor
+contract SimpleCollectible is ERC721URIStorage {
+    // ERC721URIStorage allows you to set tokenURI after minting
+    // ERC721Burnable allows you to burn the NFT if you so choose
+
     uint256 public tokenCounter;
     constructor () public ERC721 ("CollaborativeArt", "CART"){
         tokenCounter = 0;
     }
 
-    function createCollectible(string memory tokenURI) public returns (uint256) { // NFT deployer
+    // NFT deploy function that anyone can call
+    function createCollectible(string memory tokenURI) public returns (uint256) { // consider changing from public to owner for higher fidelity
         uint256 newItemId = tokenCounter;
         _safeMint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
