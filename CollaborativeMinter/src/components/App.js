e10b0d63fe7d1c3fa9d00eb15d0a04e89577245c
Gilberto Guadiana <gilbertoguadiana2022@u.northwestern.edu>
2021-08-03 12:17:13 -0500

    started react implementation

---
+++ CollaborativeMinter/src/components/App.js
@@ -0,0 +1,23 @@
+
+import React, { useState, useRef } from 'react';
+import NftInterface from "./NftInterface";
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  function handleAddTodo(e) {
+
+  }
+
+  return (
+    <>
+      <NftInterface todos={todos}/>
+      <input type="text" />
+      <button onClick={handleAddTodo}> Add Todo </button>
+      <button> Clear Completed Todos </button>
+      <div> 0 left to do </div>
+    </>
+  );
+}
+
+export default App;
