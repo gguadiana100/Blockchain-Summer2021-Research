9edbc3bea030e7f6ccc6ae760b3340ecaff013dc
Gilberto Guadiana <gilbertoguadiana2022@u.northwestern.edu>
2021-08-05 11:08:31 -0500

    fixed useState and added handleFindGame

--- CollaborativeMinter/src/components/App.js
+++ CollaborativeMinter/src/components/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Web3 from 'web3';
 import CollaborativeMinterFactory from '../build/CollaborativeMinterFactory.json'
 import CollaborativeMinter from '../build/CollaborativeMinter.json'
@@ -9,8 +9,14 @@ const ipfs = create({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
 
 function App() {
 
-  const [values, setValues] = useState({account: null, buffer: null,
-    factoryContract: null, minterContract: null, minterTokenCounter: null}); // set state variables to null
+  const [values, setValues] = useState({account: "", buffer: "Submit a picture",
+    factoryContract: "Connect a factory contract",
+    minterContract: {_address: "Connect a cominter contract"},
+    minterTokenCounter: "Connect a cominter contract",
+    cominterTurn: "Connect a cominter contract"}); // set state variables to null
+
+  const findGameRef = useRef()
+  const createGameRef = useRef()
 
   const captureFile = (event) => { // used to process image for IPFS
     event.preventDefault()
@@ -19,10 +25,9 @@ function App() {
     const reader = new window.FileReader()
     reader.readAsArrayBuffer(file)
     reader.onloadend = () => {
-      let oldValues = values;
-      oldValues.buffer = Buffer(reader.result)
-      setValues(oldValues)
-      console.log('buffer', values.buffer)
+      setValues(prevValues => {
+        return {...prevValues, buffer: Buffer(reader.result)}
+      })
     }
   }
 
@@ -38,58 +43,114 @@ function App() {
     })
   }
 
-  async function componentWillMount(){
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+  const handleFindGame = () => {
+    const minterAddress = findGameRef.current.value
+    if(parseInt(minterAddress)) { // check if we have a number
+      async function addMinterContract() {
+        const web3 = window.web3
+        const minterAbi = CollaborativeMinter.abi
+
+        const networkId = await web3.eth.net.getId()
+        const networkData = CollaborativeMinter.networks[networkId]
+
+        if(networkData){
+          const minterContract = new web3.eth.Contract(minterAbi, minterAddress)
+          const currentOwner = await minterContract.methods.currentOwner().call()
+          console.log(currentOwner)
+          console.log(minterContract)
+          setValues( prevValues => {
+            return {...prevValues, minterContract: minterContract}
+          })
+        }
+        else { // smart contract is not on network
+          setValues(prevValues => {
+            return {...prevValues, minterContract: {_address: "Input a valid cominter address"}}
+          })
+        }
+      }
+      addMinterContract()
+    }
+    else{
+      setValues(prevValues => {
+        return {...prevValues, minterContract: {_address: "Input a valid cominter address"}}
+      })
+    }
   }
 
-  async function loadWeb3(){ // connect to MetaMask or other Ethereum provider
-    if (window.ethereum){
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3){
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. Try using MetaMask.')
-    }
+  const handleCreateGame = () => {
+
+
   }
 
-  async function loadBlockchainData (){
-    const web3 = window.web3
-    // load account
-    const accounts = await web3.eth.getAccounts()
-    let oldValues = values;
-    oldValues.account = accounts[0]
-    setValues(oldValues)
-    // get info from CollaborativeMinterFactory JSON
-    const networkId = await web3.eth.net.getId()
-    const networkData = CollaborativeMinterFactory.networks[networkId]
+  useEffect(() => { // do once at startup
+    async function startup(){
+      async function loadWeb3(){ // connect to MetaMask or other Ethereum provider
+        if (window.ethereum){
+          window.web3 = new Web3(window.ethereum)
+          await window.ethereum.enable()
+        }
+        else if (window.web3){
+          window.web3 = new Web3(window.web3.currentProvider)
+        }
+        else {
+          window.alert('Non-Ethereum browser detected. Try using MetaMask.')
+        }
+      }
+      async function loadBlockchainData (){
+        const web3 = window.web3
+        // load account
+        const accounts = await web3.eth.getAccounts()
+        setValues(prevValues => {
+          return {...prevValues, account: accounts[0]}
+        })
+        // get info from CollaborativeMinterFactory JSON
+        const networkId = await web3.eth.net.getId()
+        const networkData = CollaborativeMinterFactory.networks[networkId]
 
-    if(networkData){
-      const abi = CollaborativeMinterFactory.abi
-      const address = networkData.address
-      const factoryContract = new web3.eth.Contract(abi, address)
-      console.log(factoryContract)
-      const oldValues = values;
-      oldValues.factoryContract = factoryContract
-      setValues(oldValues)
+        if(networkData){
+          const abi = CollaborativeMinterFactory.abi
+          const address = networkData.address
+          const factoryContract = new web3.eth.Contract(abi, address)
+          console.log(factoryContract)
+          setValues(prevValues => {
+            return {...prevValues, factoryContract: factoryContract}
+          })
+        }
+        else {
+          window.alert('The smart contract is not deployed on this network')
+        }
+      }
+      await loadWeb3()
+      await loadBlockchainData()
     }
-    else {
-      window.alert('The smart contract is not deployed on this network')
-    }
-  }
+    startup()
+  },[])
 
   return (
     <>
-      <input type="text" />
-      <button> Find Game </button>
-      <button> Create Game </button>
+      <div>
+        <h2>Collaborative Minter</h2>
+        <h2> Address: {values.account} </h2>
+        <h2> Factory address: {values.factoryContract._address} </h2>
+        <h3> Cominter address: {values.minterContract._address} </h3>
+        <h3> Cominter token count: {values.minterTokenCounter} </h3>
+        <h3> Cominter turn: {values.cominterTurn} </h3>
+        <h3> Loaded picture: {values.buffer} </h3>
+      </div>
+
+      <div>
+        <button onClick={handleFindGame}> Find game with address</button>
+        <input type="text" placeholder='cominter address' ref={findGameRef}/>
+        <div style={{ margin: 10 }}>
+          <button onClick={handleCreateGame}> Create Game </button>
+          <input type="text" placeholder='owners' ref={createGameRef}/>
+        </div>
+      </div>
+
       <div>
         <form onSubmit={onSubmit}>
           <input type='file' onChange={captureFile} />
-          <input type='submit' />
+          <input type='submit' value='Submit Picture'/>
         </form>
       </div>
     </>
