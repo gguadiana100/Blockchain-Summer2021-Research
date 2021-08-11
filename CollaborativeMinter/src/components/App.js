b74b0acce06ebb5b91d1c19421ed7107b5a55ab2
Gilberto Guadiana <gilbertoguadiana2022@u.northwestern.edu>
2021-08-11 11:14:52 -0500

    Refactoring and cleaning up comments

--- CollaborativeMinter/src/components/App.js
+++ CollaborativeMinter/src/components/App.js
@@ -10,20 +10,24 @@ const ipfs = create({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
 
 function App() {
 
+  // initialize state variables
   const [values, setValues] = useState({account: "Connect an ETH account", buffer: "Submit a picture",
     factoryContract: {_address: "Connect a factory contract"},
-    minterContract: {_address: "Connect a cominter contract"},
-    minterTokenCount: "Connect a cominter contract",
-    minterOwners: [],
+    cominterContract: {_address: "Connect a cominter contract"},
+    cominterTokenCount: "Connect a cominter contract",
+    cominterOwners: [],
     cominterTurn: "Connect a cominter contract",
-    ipfsHashes: [],}); // set state variables to null
+    ipfsHashes: [],});
 
-  const findGameRef = useRef()
-  const createGameRef = useRef()
+  // set references for text boxes
+  const findCominterRef = useRef()
+  const createCominterRef = useRef()
   const findFactoryRef = useRef()
   const nameSubmit = useRef()
+  const descriptionSubmit = useRef()
 
-  const captureFile = (event) => { // used to process image for IPFS
+  // used to process image for IPFS
+  const captureFile = (event) => {
     event.preventDefault()
     const file = event.target.files[0]
     console.log("Starting to process:", event.target.files[0])
@@ -36,10 +40,11 @@ function App() {
     }
     console.log("Finished processing file")
   }
-
- const onSubmit = async (event) => { // used to send image to IPFS
+ // used to send image to IPFS and submit to the cominter
+ const onSubmit = async (event) => {
     event.preventDefault()
-    if(values.cominterTurn != values.account){ // check if it is your turn to submit
+    console.log("Current name: ", nameSubmit.current.value)
+    if(values.cominterTurn !== values.account){ // check if it is your turn to submit
       console.log("It is not your turn")
       return
     }
@@ -50,14 +55,17 @@ function App() {
     console.log("Creating collectible")
 
     const web3 = window.web3
-    const minterContract = values.minterContract;
+    const cominterContract = values.cominterContract;
 
     // get the information for the metadata JSON
-    const description = "This stage of the artwork was submitted by " + values.account
+    const authorDescription = "This stage of the artwork was submitted by " + values.account + ". "
     let imageURL = "https://ipfs.io/ipfs/" + result.path // IPFS URL
     const image = imageURL
     const name = nameSubmit.current.value // get the name input
+    const description = authorDescription + descriptionSubmit.current.value // get the description input
+    console.log("Description: ", description)
 
+    // create metadata JSON for IPFS
     const metadataJson = JSON.stringify({
       "description": description,
       "image": image,
@@ -66,30 +74,33 @@ function App() {
 
     console.log("Submitting metadata to IPFS...")
     const metadataResult = await ipfs.add(metadataJson)
-    console.log("IPFS result of metadata", metadataResult)
+    console.log("IPFS result of metadata: ", metadataResult)
 
     let metadataUri = "https://ipfs.io/ipfs/" + metadataResult.path
     console.log(metadataUri)
-    let newTokenTransaction = await minterContract.methods.collaborativeMint(metadataUri).send( // create the collectible
+
+    // create the collectible
+    let newTokenTransaction = await cominterContract.methods.collaborativeMint(metadataUri).send(
       {from: values.account},
       function(error, result){
         return result
       })
 
     // update the state based on the smart contract values
-    const cominterTurnIndex = await minterContract.methods.currentOwner().call()
-    const cominterTurn = await minterContract.methods.owners(cominterTurnIndex).call()
-    const minterTokenCount = await minterContract.methods.tokenCounter().call()
+    const cominterTurnIndex = await cominterContract.methods.currentOwner().call()
+    const cominterTurn = await cominterContract.methods.owners(cominterTurnIndex).call()
+    const cominterTokenCount = await cominterContract.methods.tokenCounter().call()
     console.log("Finished creating collectible")
 
     setValues(prevValues => {
       return {...prevValues,
         ipfsHashes: [...prevValues.ipfsHashes, result.path, metadataResult.path],
         cominterTurn: cominterTurn,
-        minterTokenCount: minterTokenCount}
+        cominterTokenCount: cominterTokenCount}
     })
   }
 
+  // used to debug IPFS
   const displayLatestIPFS = async (event) => {
     const length = values.ipfsHashes.length
 
@@ -110,36 +121,36 @@ function App() {
     }
   }
 
-  const handleFindGame = () => {
-    const minterAddress = findGameRef.current.value
+  const handleFindCominter = () => {
+    const minterAddress = findCominterRef.current.value
     if(parseInt(minterAddress)) { // check if we have a number
-      async function addMinterContract() {
+      async function addcominterContract() {
         const web3 = window.web3
         const minterAbi = CollaborativeMinter.abi
 
         try{
-          const minterContract = new web3.eth.Contract(minterAbi, minterAddress)
+          const cominterContract = new web3.eth.Contract(minterAbi, minterAddress)
           // test to see if the minter contract is valid
-          const cominterTurnIndex = await minterContract.methods.currentOwner().call()
-          const cominterTurn = await minterContract.methods.owners(cominterTurnIndex).call()
-          const minterTokenCount = await minterContract.methods.tokenCounter().call()
+          const cominterTurnIndex = await cominterContract.methods.currentOwner().call()
+          const cominterTurn = await cominterContract.methods.owners(cominterTurnIndex).call()
+          const cominterTokenCount = await cominterContract.methods.tokenCounter().call()
 
           setValues(prevValues => {
-            return {...prevValues, minterContract: minterContract, cominterTurn: cominterTurn,
-              minterTokenCount: minterTokenCount}
+            return {...prevValues, cominterContract: cominterContract, cominterTurn: cominterTurn,
+              cominterTokenCount: cominterTokenCount}
           })
         }
         catch { // smart contract is not on network
           setValues(prevValues => {
-            return {...prevValues, minterContract: {_address: "Input a valid cominter address"}}
+            return {...prevValues, cominterContract: {_address: "Input a valid cominter address"}}
           })
         }
       }
-      addMinterContract()
+      addcominterContract()
     }
     else{
       setValues(prevValues => {
-        return {...prevValues, minterContract: {_address: "Input a valid cominter address"}}
+        return {...prevValues, cominterContract: {_address: "Input a valid cominter address"}}
       })
     }
   }
@@ -174,7 +185,7 @@ function App() {
     }
   }
 
-  const handleCreateGame = async () => {
+  const handleCreateCominter = async () => {
     if(values.factoryContract._address === "Connect a factory contract"){
       console.log("add factory contract")
       return
@@ -183,7 +194,7 @@ function App() {
     let _owners
 
     try{
-      _owners = JSON.parse(createGameRef.current.value)
+      _owners = JSON.parse(createCominterRef.current.value)
     }
     catch{
       console.log("provide valid input")
@@ -194,46 +205,46 @@ function App() {
     const factoryContract = values.factoryContract;
 
     // deploy the collaborative minter
-    const newGameAddressTransaction = await factoryContract.methods.createCollaborativeMinter(_owners).send(
+    const newCominterAddressTransaction = await factoryContract.methods.createCollaborativeMinter(_owners).send(
       {from: values.account},
       function(error, result){
         return result
       })
-    const newGameIndex = await factoryContract.methods.getNumberOfCollaborativeMinters(values.account).call() - 1
-    const newGameAddressIndex = await factoryContract.methods.accountToCominters(values.account,newGameIndex).call()
-    const newGameAddress = await factoryContract.methods.collaborativeMinters(newGameAddressIndex).call()
+    const newCominterIndex = await factoryContract.methods.getNumberOfCollaborativeMinters(values.account).call() - 1
+    const newCominterAddressIndex = await factoryContract.methods.accountToCominters(values.account,newCominterIndex).call()
+    const newCominterAddress = await factoryContract.methods.collaborativeMinters(newCominterAddressIndex).call()
 
     const minterAbi = CollaborativeMinter.abi
-    const minterContract = new web3.eth.Contract(minterAbi, newGameAddress)
-    const cominterTurnIndex = await minterContract.methods.currentOwner().call()
-    const cominterTurn = await minterContract.methods.owners(cominterTurnIndex).call()
-    const minterTokenCount = await minterContract.methods.tokenCounter().call()
+    const cominterContract = new web3.eth.Contract(minterAbi, newCominterAddress)
+    const cominterTurnIndex = await cominterContract.methods.currentOwner().call()
+    const cominterTurn = await cominterContract.methods.owners(cominterTurnIndex).call()
+    const cominterTokenCount = await cominterContract.methods.tokenCounter().call()
 
     setValues(prevValues => {
-      return {...prevValues, minterContract: minterContract, cominterTurn: cominterTurn,
-        minterTokenCount: minterTokenCount}
+      return {...prevValues, cominterContract: cominterContract, cominterTurn: cominterTurn,
+        cominterTokenCount: cominterTokenCount}
     })
 
   }
 
-  const handleLoadGame = async () => {
-    if(values.minterContract._address === "Connect a cominter contract"){
-      console.log("Create a game first!")
+  const handleLoadCominter = async () => {
+    if(values.cominterContract._address === "Connect a cominter contract"){
+      console.log("Create a cominter first!")
       return
     }
 
     const web3 = window.web3
     const factoryContract = values.factoryContract;
-    const minterContract = values.minterContract
+    const cominterContract = values.cominterContract
 
-    const numTurns = values.minterTokenCount
+    const numTurns = values.cocominterTokenCount
     let paths = []
     console.log("Beginning to display tokenURIs")
     for (let i = 0; i < numTurns; i++){ // iterate through the deployed NFTs of the minter
-      let uri = await minterContract.methods.tokenURI(i).call()
-      // console.log(uri) // IPFS link to the metadata
+      let uri = await cominterContract.methods.tokenURI(i).call()
+      console.log(uri) // IPFS link to the metadata
       $.getJSON(uri, function(data){
-        if(data != null){
+        if(data !== null){
           let imageUrl = data.image
           let turn = document.createElement("p"); // Add sequence of images to the DOM
           let text = "Turn: " + i;
@@ -243,6 +254,7 @@ function App() {
           let img = document.createElement("img");
           img.src = imageUrl;
           document.body.appendChild(img);
+          console.log(imageUrl)
         }
       })
 
@@ -292,7 +304,6 @@ function App() {
       }
       await loadWeb3()
       await loadBlockchainData()
-
     }
     startup()
   },[])
@@ -308,8 +319,8 @@ function App() {
         </p>
 
         <p>
-          <b> Cominter address: </b> {values.minterContract._address}
-          <b> Cominter token count: </b> {values.minterTokenCount}
+          <b> Cominter address: </b> {values.cominterContract._address}
+          <b> Cominter token count: </b> {values.cocominterTokenCount}
           <b> Cominter turn: </b> {values.cominterTurn}
         </p>
         <p>
@@ -319,27 +330,28 @@ function App() {
       </div>
 
       <div>
-        <button onClick={handleFindGame}> Find game with address</button>
-        <input type="text" placeholder='cominter address' ref={findGameRef}/>
+        <button onClick={handleFindCominter}> Find cominter with address</button>
+        <input type="text" placeholder='cominter address' ref={findCominterRef}/>
         <button onClick={handleFindFactory}> Find factory with address</button>
         <input type="text" placeholder='factory address' ref={findFactoryRef}/>
         <div style={{ margin: 10 }}>
-          <button onClick={handleCreateGame}> Create Game </button>
-          <input type="text" placeholder='["owner0Address",...]' ref={createGameRef}/>
-          <button onClick={handleLoadGame}> Load Game </button>
+          <button onClick={handleCreateCominter}> Create Cominter </button>
+          <input type="text" placeholder='["owner0Address",...]' ref={createCominterRef}/>
+          <button onClick={handleLoadCominter}> Load Cominter </button>
         </div>
       </div>
 
       <div>
         <p>
-          <b> Submit to the game </b>
+          <b> Submit to the cominter </b>
         </p>
         <form onSubmit={onSubmit}>
           <input type='file' onChange={captureFile} />
           <input type='text' placeholder='name of your work' ref={nameSubmit}/>
+          <input type='text' placeholder='description of your work' ref={descriptionSubmit}/>
           <input type='submit' value='Submit your turn'/>
         </form>
-        <button style={{ margin: 10 }} onClick={displayLatestIPFS}> Display latest from IPFS </button>
+        // <button style={{ margin: 10 }} onClick={displayLatestIPFS}> Display latest from IPFS </button>
       </div>
     </>
   );
